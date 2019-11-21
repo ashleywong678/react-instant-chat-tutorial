fdf5d3f93f2c302fc8e01cf60c3951f349f77ce8
Ashley <ashleywong678@gmail.com>
2019-11-21 10:20:55 -0500

    built username form and added listener methods

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,26 @@ import ChatApp from './ChatApp';
 // This is the first screen seen by the user. We should display some way for
 // them to enter their name and enter the chat room
 class App extends React.Component {
+
+
+  handleChange = (event) => {
+    this.setState({ username: event.target.value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({ submitted: true, username: this.state.username})
+  }
+
   render() {
-    // Display a simple login screen with a username field and a submit button
+    return(
+      <form onSubmit={this.handleSubmit} className='username-container'>
+        <h1>React Instant Chat</h1>
+        <div>
+          <input type='text' onChange={this.handleChange} placeholder="Enter username..." required />
+        </div>
+      </form>
+    )
   }
 
 }
